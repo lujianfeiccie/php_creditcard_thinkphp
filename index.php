bbe3bec6e8af9419c260ca908866f34ab615f68f
lujianfeiccie <lujianfeiccie@gmail.com>
2013-12-12 23:39:34 +0800

    add sae support

--- index.php
+++ index.php
@@ -2,5 +2,9 @@
 define('APP_NAME','Index');
 define('APP_PATH','./Index/');
 define('APP_DEBUG',TRUE);
-include './ThinkPHP/ThinkPHP.php';//引入ThinkPHP核心运行文件
+if(defined("SAE_MYSQL_DB")){ //SAE环境
+	include './ThinkPHP/Extend/Engine/Sae.php';
+}else{
+	include './ThinkPHP/ThinkPHP.php';//引入ThinkPHP核心运行文件
+}
 ?>
