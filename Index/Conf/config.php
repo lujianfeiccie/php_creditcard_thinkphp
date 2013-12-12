9302ee72004af00688c9f24cb269e0e57d1bbe9c
lujianfeiccie <lujianfeiccie@gmail.com>
2013-12-12 23:09:59 +0800

    first commit

---
+++ Index/Conf/config.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+//数据库连接参数
+		'DB_HOST'=>'127.0.0.1',
+		'DB_USER'=>'root',
+		'DB_PWD'=>'123456',
+		'DB_NAME'=>'db_creditcard',
+		'DB_PREFIX'=>'tb_'
+);
+?>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+define('APP_NAME','Index');
+define('APP_PATH','./Index/');
+define('APP_DEBUG',TRUE);
+include './ThinkPHP/ThinkPHP.php';//引入ThinkPHP核心运行文件
+?>
